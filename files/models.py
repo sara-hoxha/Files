4006da459b1bfdd1d96b2193cd51872619ec9cb4
Sara Work <sarahoxha.work@gmail.com>
2024-11-26 10:36:58 +0100

    modify models.py

--- files/models.py
+++ files/models.py
@@ -4,3 +4,6 @@ class File(models.Model):
     name = models.CharField(max_length=500)
     file_type = models.CharField(max_length=500)
     upload_timestamp = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.name
