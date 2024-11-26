b85cf482ca65827e7db928c69783145ac8ed27ba
Sara Work <sarahoxha.work@gmail.com>
2024-11-26 09:57:10 +0100

    add models.py

---
+++ files/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class File(models.Model):
+    name = models.CharField(max_length=500)
+    file_type = models.CharField(max_length=500)
+    upload_timestamp = models.IntegerField(default=0)
